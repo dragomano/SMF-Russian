605e3f3cb6ad5f38938c9ede9449cba545e375ac
Bugo <bugo@dragomano.ru>
2022-02-19 11:52:23 +0500

    Change compression method

--- create_packages.php
+++ create_packages.php
@@ -6,16 +6,14 @@ $zipFile = new \PhpZip\ZipFile();
 
 try {
     $zipFile
-        ->addDirRecursive(__DIR__ . '/dist_install', '/', \PhpZip\Constants\ZipCompressionMethod::BZIP2)
+        ->addDirRecursive(__DIR__ . '/dist_install', '/', \PhpZip\Constants\ZipCompressionMethod::DEFLATED)
         ->saveAsFile('smf21_rus_install.zip')
         ->close();
 
     $zipFile
-        ->addDirRecursive(__DIR__ . '/dist_upgrade', '/', \PhpZip\Constants\ZipCompressionMethod::BZIP2)
+        ->addDirRecursive(__DIR__ . '/dist_upgrade', '/', \PhpZip\Constants\ZipCompressionMethod::DEFLATED)
         ->saveAsFile('smf21_rus_upgrade.zip')
         ->close();
 } catch(\PhpZip\Exception\ZipException $e) {
     file_put_contents('error.log', $e->getMessage());
-} finally {
-    $zipFile->close();
 }
