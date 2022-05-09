8e15db480bce6677d04b86518c5ca29b4982401a
Bugo <bugo@dragomano.ru>
2022-05-09 21:39:51 +0500

    Move all tweaks to apply_tweaks.php

---
+++ apply_tweaks.php
@@ -0,0 +1,40 @@
+<?php
+
+// Отключаем сбор статистики по умолчанию
+$install = file_get_contents(__DIR__ . '/dist_install/install.php');
+$install = str_replace('name="stats" id="stats_check" checked="checked"', 'name="stats" id="stats_check"', $install);
+
+file_put_contents(__DIR__ . '/dist_install/install.php', $install);
+
+// Твики в SQL файлах
+$tweaks = array(
+	// Отключаем проверку пароля при входе в админку
+	// Сортируем часовые пояса
+	// Отключаем отображение заголовков сообщений
+	// Подгружаем локальный jQuery вместо CDN
+	"('securityDisable_moderate', '1')" => "('securityDisable_moderate', '1'),
+	('securityDisable', '1'),
+	('timezone_priority_countries', 'RU'),
+	('subject_toggle', '0'),
+	('jquery_source', 'local')",
+
+	// Меняем ICQ на Telegram
+	"'cust_icq', '{icq}', '{icq_desc}', 'text', 12, '', 1, 'regex~[1-9][0-9]{4,9}~i'" => "'cust_tg', '{telegram}', '{telegram_desc}', 'text', 32, '', 1, 'nohtml'",
+	'<a class="icq" href="//www.icq.com/people/{INPUT}" target="_blank" rel="noopener" title="ICQ - {INPUT}"><img src="{DEFAULT_IMAGES_URL}/icq.png" alt="ICQ - {INPUT}"></a>' => '<a href="//t.me/{INPUT}" target="_blank" rel="noopener" title="Telegram - {INPUT}"><img src="{DEFAULT_IMAGES_URL}/telegram-icon.png" alt="Telegram - {INPUT}"></a>',
+
+	// Делаем Понедельник первым днём недели
+	"(-1, 1, 'return_to_post', '1')" => "(-1, 1, 'return_to_post', '1'),
+	(-1, 1, 'calendar_start_day', '1')",
+);
+
+// MySQL
+$mysql = file_get_contents(__DIR__ . '/dist_install/install_2-1_mysql.sql');
+$mysql = strtr($mysql, $tweaks);
+
+file_put_contents(__DIR__ . '/dist_install/install_2-1_mysql.sql', $mysql);
+
+// PostreSQL
+$pgsql = file_get_contents(__DIR__ . '/dist_install/install_2-1_postgresql.sql');
+$pgsql = strtr($pgsql, $tweaks);
+
+file_put_contents(__DIR__ . '/dist_install/install_2-1_postgresql.sql', $pgsql);
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,6 @@ mix.copy('vendor/simplemachines/smf-russian', 'dist_upgrade');
 // Tweaks
 mix.copy('tweaks', 'dist_install');
 mix.copy('tweaks/readme.html', 'dist_upgrade/readme.html');
-mix.copy('tweaks/Themes/default/PersonalMessage.template.php', 'dist_upgrade/Themes/default/PersonalMessage.template.php');
-mix.copy('tweaks/Sources/Subs.php', 'dist_upgrade/Sources/Subs.php');
 mix.copy('node_modules/jquery/dist/jquery.min.js', 'dist_install/Themes/default/scripts/jquery-3.6.0.min.js');
 mix.copy('node_modules/jquery/dist/jquery.min.js', 'dist_upgrade/Themes/default/scripts/jquery-3.6.0.min.js');
 mix.copy('node_modules/sceditor/minified/jquery.sceditor.bbcode.min.js', 'dist_install/Themes/default/scripts/jquery.sceditor.bbcode.min.js');
