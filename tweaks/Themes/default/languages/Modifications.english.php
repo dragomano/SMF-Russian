1d3ac969ab9ddd536556bf95b8b7aa23ae851c76
Bugo <cheetah@bk.ru>
2022-11-21 23:51:01 +0500

    Update to 2.1.3

--- tweaks/Themes/default/languages/Modifications.english.php
+++ tweaks/Themes/default/languages/Modifications.english.php
@@ -1,5 +1,5 @@
 <?php
-// Version: 2.1.0; Modifications
+// Version: 2.1.3; Modifications
 
 $txt['telegram'] = 'Telegram';
 $txt['telegram_desc'] = 'Your nickname in Telegram.';
--- tweaks/Themes/default/languages/Modifications.russian.php
+++ tweaks/Themes/default/languages/Modifications.russian.php
@@ -1,5 +1,5 @@
 <?php
-// Version: 2.1.0; Modifications
+// Version: 2.1.3; Modifications
 
 $txt['telegram'] = 'Telegram';
 $txt['telegram_desc'] = 'Ваш ник в Telegram.';
