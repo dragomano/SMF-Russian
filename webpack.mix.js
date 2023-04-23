8c006fe575c98e45990a40fd09e2058f004161d2
Bugo <cheetah@bk.ru>
2023-04-23 09:49:48 +0500

    Update to 2.1.4

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ mix.copy('vendor/simplemachines/smf-russian', 'dist_upgrade');
 // Tweaks
 mix.copy('tweaks', 'dist_install');
 mix.copy('tweaks/readme.html', 'dist_upgrade/readme.html');
-mix.copy('node_modules/jquery/dist/jquery.min.js', 'dist_install/Themes/default/scripts/jquery-3.6.0.min.js');
-mix.copy('node_modules/jquery/dist/jquery.min.js', 'dist_upgrade/Themes/default/scripts/jquery-3.6.0.min.js');
+mix.copy('node_modules/jquery/dist/jquery.min.js', 'dist_install/Themes/default/scripts/jquery-3.6.3.min.js');
+mix.copy('node_modules/jquery/dist/jquery.min.js', 'dist_upgrade/Themes/default/scripts/jquery-3.6.3.min.js');
 mix.copy('node_modules/sceditor/minified/jquery.sceditor.bbcode.min.js', 'dist_install/Themes/default/scripts/jquery.sceditor.bbcode.min.js');
 mix.copy('node_modules/sceditor/minified/jquery.sceditor.bbcode.min.js', 'dist_upgrade/Themes/default/scripts/jquery.sceditor.bbcode.min.js');
