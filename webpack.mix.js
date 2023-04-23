3784aa519a7642bf0533236852d7c59be20049a9
Bugo <cheetah@bk.ru>
2023-04-23 10:01:21 +0500

    Remove SCEditor from build

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,3 @@ mix.copy('tweaks', 'dist_install');
 mix.copy('tweaks/readme.html', 'dist_upgrade/readme.html');
 mix.copy('node_modules/jquery/dist/jquery.min.js', 'dist_install/Themes/default/scripts/jquery-3.6.3.min.js');
 mix.copy('node_modules/jquery/dist/jquery.min.js', 'dist_upgrade/Themes/default/scripts/jquery-3.6.3.min.js');
-mix.copy('node_modules/sceditor/minified/jquery.sceditor.bbcode.min.js', 'dist_install/Themes/default/scripts/jquery.sceditor.bbcode.min.js');
-mix.copy('node_modules/sceditor/minified/jquery.sceditor.bbcode.min.js', 'dist_upgrade/Themes/default/scripts/jquery.sceditor.bbcode.min.js');
